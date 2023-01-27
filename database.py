78033dbe84421ee4eba6b3c7127c402392b91505
Dawid Makówka <95069336+vexterq@users.noreply.github.com>
2023-01-27 14:23:24 +0100

    update database.py

--- database.py
+++ database.py
@@ -1,14 +1,21 @@
+"""This module contains the Database class"""
 import sqlite3
 
 
 class Database:
-    def __init__(self, database):
+    """This class represents a database."""
+
+    def __init__(self, database: str):
+        """Initialize the database connection.
+            Args: database(str): The path to the database file."""
         self.connection = sqlite3.connect(database)
 
-    def create_table(self):
+    def _create_table(self):
+        """Create a table in the database."""
         with self.connection as conn:
             conn.execute('''CREATE TABLE IF NOT EXISTS contacts(
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 name VARCHAR,
                 phone_number VARCHAR(9),
                 email VARCHAR)''')
+
